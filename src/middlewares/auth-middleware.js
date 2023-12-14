63ad0943fe07e1fb49c098596de93c1300fad5fe
Bryan Enid <bryanenid97@gmail.com>
2023-12-14 10:43:04 -0500

    still working

--- src/middlewares/auth-middleware.js
+++ src/middlewares/auth-middleware.js
@@ -1,30 +1,23 @@
 const Boom = require('boom');
-// const { jwtDecode } = require('jwt-decode');
+const { jwtDecode } = require('jwt-decode');
 
 const { getUserById } = require('../db/users');
 const { logger } = require('../utils/logger');
 
 const authMiddleware = () => async (req, res, next) => {
   const authorization = req.headers.authorization || '';
-  if (!authorization) {
-    return next(Boom.unauthorized());
-  }
 
-  let user;
-  try {
-    const id = authorization.replace('Bearer ', '').trim();
-    user = await getUserById(id);
-  } catch (e) {
-    logger().error(e);
-  }
+  if (!authorization) return next(Boom.unauthorized());
 
-  if (!user) {
-    return next(Boom.unauthorized());
-  }
+  const { user_id: id } = jwtDecode(authorization);
 
-  req.context = {
-    userId: user.id,
-  };
+  getUserById(id)
+    .then((user) => {
+      req.context = { userId: user.id };
+      if (!user) return next(Boom.unauthorized());
+      return next();
+    })
+    .catch((e) => logger().error(e));
 
   return next();
 };
