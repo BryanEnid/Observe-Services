2f99ebd6998aa0fdaf5bc968c1d48c74756c0aac
Oleh Pylypenko <oleh.pylypenko@hygge.software>
2023-12-14 17:17:14 +0200

    Move firebase config to env

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,9 @@
 const admin = require('firebase-admin');
+const config = require('config');
 
-const serviceAccount = require('../config/firebase-config.json');
+const serviceAccount = {
+  ...config.get('firebase'),
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
