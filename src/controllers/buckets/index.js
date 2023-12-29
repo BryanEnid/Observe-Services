193fbd5fee6b9168d8dba903e214ee9244a2cee7
Bryan Enid <bryanenid97@gmail.com>
2023-12-28 21:09:16 -0500

    GET should be public

--- src/controllers/buckets/index.js
+++ src/controllers/buckets/index.js
@@ -40,99 +40,110 @@ const { deleteAnswerRoute: deletePollAnswerRoute } = require('./polls/delete-ans
 
 const router = express.Router();
 
-router.use(authMiddleware());
+// router.use(authMiddleware());
 router.get('/', celebrate(listRouteValidator, defaultSchemaOptions), errorWrapper(getBucketsListRoute));
 router.get('/:id', errorWrapper(getBucketByIdRoute));
-router.post('/', express.json(), celebrate(createBucketValidation, defaultSchemaOptions), errorWrapper(createBucketRoute));
-router.put('/:id', express.json(), celebrate(updateBucketValidation, defaultSchemaOptions), errorWrapper(updateBucketRoute));
-router.delete('/:id', errorWrapper(deleteBucketRoute));
+router.post(
+  '/',
+  authMiddleware(),
+  express.json(),
+  celebrate(createBucketValidation, defaultSchemaOptions),
+  errorWrapper(createBucketRoute)
+);
+router.put(
+  '/:id',
+  authMiddleware(),
+  express.json(),
+  celebrate(updateBucketValidation, defaultSchemaOptions),
+  errorWrapper(updateBucketRoute)
+);
+router.delete('/:id', authMiddleware(), errorWrapper(deleteBucketRoute));
 
 router.post(
   '/:id/video',
-  uploadToS3.fields([{ name: 'video', maxCount: 1 }, { name: 'image', maxCount: 1 }]),
-  errorWrapper(uploadVideoRoute),
+  authMiddleware(),
+  uploadToS3.fields([
+    { name: 'video', maxCount: 1 },
+    { name: 'image', maxCount: 1 },
+  ]),
+  errorWrapper(uploadVideoRoute)
 );
 router.post(
   '/:id/process',
-  uploadLocal.fields([{ name: 'mainVideo', maxCount: 1 }, { name: 'subVideo', maxCount: 1 }, { name: 'image', maxCount: 1 }]),
+  authMiddleware(),
+  uploadLocal.fields([
+    { name: 'mainVideo', maxCount: 1 },
+    { name: 'subVideo', maxCount: 1 },
+    { name: 'image', maxCount: 1 },
+  ]),
   celebrate(uploadAndProcessValidation),
-  errorWrapper(uploadAndProcessRoute),
+  errorWrapper(uploadAndProcessRoute)
 );
 
-router.get(
-  '/:id/videos/:videoId/questions',
-  errorWrapper(getQuestionsListRoute),
-);
+router.get('/:id/videos/:videoId/questions', errorWrapper(getQuestionsListRoute));
 router.post(
   '/:id/videos/:videoId/questions',
+  authMiddleware(),
   express.json(),
   celebrate(createQuestionValidation, defaultSchemaOptions),
-  errorWrapper(createQuestionRoute),
+  errorWrapper(createQuestionRoute)
 );
 router.put(
   '/:id/videos/:videoId/questions/:questionId',
+  authMiddleware(),
   express.json(),
   celebrate(updateQuestionValidation, defaultSchemaOptions),
-  errorWrapper(updateQuestionRoute),
+  errorWrapper(updateQuestionRoute)
 );
-router.delete(
-  '/:id/videos/:videoId/questions/:questionId',
-  errorWrapper(deleteQuestionRoute),
-);
+router.delete('/:id/videos/:videoId/questions/:questionId', authMiddleware(), errorWrapper(deleteQuestionRoute));
 
 router.post(
   '/:id/videos/:videoId/questions/:questionId/answer',
+  authMiddleware(),
   express.json(),
   celebrate(createAnswerValidation),
-  errorWrapper(createAnswerRoute),
+  errorWrapper(createAnswerRoute)
 );
 router.put(
   '/:id/videos/:videoId/questions/:questionId/answer',
+  authMiddleware(),
   express.json(),
   celebrate(updateAnswerValidation),
-  errorWrapper(updateAnswerRoute),
+  errorWrapper(updateAnswerRoute)
 );
-router.delete(
-  '/:id/videos/:videoId/questions/:questionId/answer',
-  errorWrapper(deleteAnswerRoute),
-);
+router.delete('/:id/videos/:videoId/questions/:questionId/answer', authMiddleware(), errorWrapper(deleteAnswerRoute));
 
-router.get(
-  '/:id/videos/:videoId/polls',
-  errorWrapper(getPollsListRoute),
-);
+router.get('/:id/videos/:videoId/polls', errorWrapper(getPollsListRoute));
 router.post(
   '/:id/videos/:videoId/polls',
+  authMiddleware(),
   express.json(),
   celebrate(createPollValidation, defaultSchemaOptions),
-  errorWrapper(createPollRoute),
+  errorWrapper(createPollRoute)
 );
 router.put(
   '/:id/videos/:videoId/polls/:pollId',
+  authMiddleware(),
   express.json(),
   celebrate(updatePollValidation, defaultSchemaOptions),
-  errorWrapper(updatePollRoute),
+  errorWrapper(updatePollRoute)
 );
-router.delete(
-  '/:id/videos/:videoId/polls/:pollId',
-  errorWrapper(deletePollRoute),
-);
+router.delete('/:id/videos/:videoId/polls/:pollId', errorWrapper(deletePollRoute));
 
 router.post(
   '/:id/videos/:videoId/polls/:pollId/answer',
+  authMiddleware(),
   express.json(),
   celebrate(createPollAnswerValidation),
-  errorWrapper(createPollAnswerRoute),
+  errorWrapper(createPollAnswerRoute)
 );
 router.put(
   '/:id/videos/:videoId/polls/:pollId/answer',
+  authMiddleware(),
   express.json(),
   celebrate(updatePollAnswerValidation),
-  errorWrapper(updatePollAnswerRoute),
+  errorWrapper(updatePollAnswerRoute)
 );
-router.delete(
-  '/:id/videos/:videoId/polls/:pollId/answer',
-  errorWrapper(deletePollAnswerRoute),
-);
+router.delete('/:id/videos/:videoId/polls/:pollId/answer', authMiddleware(), errorWrapper(deletePollAnswerRoute));
 
 module.exports = router;
