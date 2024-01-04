51ff71da8ae65242dc806fd9d54592e01d864eca
Bryan Enid <bryanenid97@gmail.com>
2024-01-04 12:47:19 -0500

    removed title to be required, and category will have a different flow on the ui

--- src/controllers/buckets/update.js
+++ src/controllers/buckets/update.js
@@ -10,23 +10,26 @@ const updateBucketValidation = {
     id: Joi.string().optional(),
     creatorId: Joi.string().optional(),
     name: Joi.string().optional(),
-    title: Joi.string().optional(),
+    title: Joi.string().optional().allow(null, ''),
     description: Joi.any().optional(),
     category: Joi.string().optional(),
-    videos: Joi.array().items(
-      Joi.object({
-        id: Joi.string().required(),
-        image: Joi.string().optional(),
-        videoUrl: Joi.string().optional(),
-        chosen: Joi.boolean().optional().allow(null),
-        selected: Joi.boolean().optional().allow(null),
-        description: Joi.string().optional().allow(null, ''),
-        pollQuestions: Joi.boolean().optional(),
-        views: Joi.number().optional(),
-        contributors: Joi.array().items(Joi.string().optional()).optional(),
-        videoType: Joi.string().optional().allow(null, ''),
-      }),
-    ).optional().allow(null),
+    videos: Joi.array()
+      .items(
+        Joi.object({
+          id: Joi.string().required(),
+          image: Joi.string().optional(),
+          videoUrl: Joi.string().optional(),
+          chosen: Joi.boolean().optional().allow(null),
+          selected: Joi.boolean().optional().allow(null),
+          description: Joi.string().optional().allow(null, ''),
+          pollQuestions: Joi.boolean().optional(),
+          views: Joi.number().optional(),
+          contributors: Joi.array().items(Joi.string().optional()).optional(),
+          videoType: Joi.string().optional().allow(null, ''),
+        })
+      )
+      .optional()
+      .allow(null),
   }).required(),
 };
 
@@ -40,9 +43,7 @@ const updateBucketRoute = async (req, res, next) => {
   const {
     context: { userId },
     params: { id },
-    body: {
-      id: _id, creatorId, videos, ...data
-    },
+    body: { id: _id, creatorId, videos, ...data },
   } = req;
 
   const bucket = await findBucket({ _id: id, creatorId: userId });
@@ -53,15 +54,18 @@ const updateBucketRoute = async (req, res, next) => {
   let videoToDelete;
   if (videos && Array.isArray(videos)) {
     videoToDelete = bucket.videos?.filter(
-      ({ id: existingVideoId }) => !videos?.find(({ id: videoId }) => existingVideoId === videoId),
+      ({ id: existingVideoId }) => !videos?.find(({ id: videoId }) => existingVideoId === videoId)
     );
     if (videoToDelete?.length) {
       await Promise.all(
-        videoToDelete.reduce((acc, { image, videoUrl }) => ([
-          ...acc,
-          ...(image ? [deleteObject(getFileNameFromUrl(image))] : []),
-          ...(videoUrl ? [deleteObject(getFileNameFromUrl(videoUrl))] : []),
-        ]), []),
+        videoToDelete.reduce(
+          (acc, { image, videoUrl }) => [
+            ...acc,
+            ...(image ? [deleteObject(getFileNameFromUrl(image))] : []),
+            ...(videoUrl ? [deleteObject(getFileNameFromUrl(videoUrl))] : []),
+          ],
+          []
+        )
       );
     }
   }
